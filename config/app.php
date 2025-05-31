1b4f28c70d58510cede9f7f88227ce369726bc63
santeyx <alexandr.yakovenko07@gmail.com>
2025-05-31 22:55:34 +0300

    init

--- config/app.php
+++ config/app.php
@@ -54,6 +54,10 @@ return [
 
     'url' => env('APP_URL', 'http://localhost'),
 
+    'domain' => env('APP_DOMAIN', ''),
+
+    'telegram_token' => env('APP_TELEGRAM_TOKEN', ''),
+
     'asset_url' => env('ASSET_URL', null),
 
     /*
